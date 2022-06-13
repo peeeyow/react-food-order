e958f92f7fc93b6ca2c1910173526efd078d7530
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-06-13 21:18:02 +0800

    Implement AvailableMeals component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import HeaderImage from "./components/Header/HeaderImage";
 import Header from "./components/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
       <HeaderImage />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+const MealSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Goods Karinderya Meals</h2>
+      <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint</p>
+    </section>
+  );
+};
+
+export default MealSummary;
